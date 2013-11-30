b5e54934832c67ee1e0f89168e69dac576e4c2db
Gizburdt <gizburdt@gmail.com>
2013-11-30 23:41:26 +0100

    Child theme files

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/*==================================================*/
+/* Sidebars, widgets
+/*==================================================*/
+	
+// Register sidebars
+function scaffold_register_extra_sidebars()
+{
+}
+
+// Register widgets
+function scaffold_register_widgets()
+{
+}
