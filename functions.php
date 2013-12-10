6e841d309612bf2eb523a94a873cf5066c08481d
Gizburdt <gizburdt@gmail.com>
2013-12-10 22:47:09 +0100

    Big cleanup
    Needs more improvement, but can be used out of the box

--- functions.php
+++ functions.php
@@ -1,15 +1,37 @@
 <?php
 
-/*==================================================*/
-/* Sidebars, widgets
-/*==================================================*/
-	
-// Register sidebars
-function scaffold_register_extra_sidebars()
-{
+// Block direct access
+if( ! defined( 'ABSPATH' ) ) exit;
+
+// Error reporting
+error_reporting( E_ALL );
+ini_set( 'display_errors', 1 );
+
+// Set content_width
+if ( ! isset( $content_width ) ) {
+	$content_width = 640;
 }
 
-// Register widgets
-function scaffold_register_widgets()
-{
+// Assets/vendor dir
+if( ! defined( 'SCAFFOLD_VENDOR_DIR' ) ) {
+	define( 'SCAFFOLD_VENDOR_DIR', WP_CONTENT_URL . '/vendor' );
 }
+
+/**
+ * Excerpt length
+ */
+function scaffold_excerpt_length( $length ) 
+{
+	return 55;
+}
+add_filter( 'excerpt_length', 'scaffold_excerpt_length' );
+
+/**
+ * Exceprt more link
+ */
+function scaffold_excerpt_more( $more ) 
+{
+	return ' [...]';
+}
+add_filter( 'excerpt_more', 'scaffold_excerpt_more' );
+
