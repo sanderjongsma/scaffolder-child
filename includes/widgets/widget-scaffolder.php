2ade896165b138e4f90ee3c48c0e8e33f9362ce2
Gijs Jorissen <gizburdt@gmail.com>
2016-03-21 20:49:13 +0100

    Code style

--- includes/widgets/widget-scaffolder.php
+++ includes/widgets/widget-scaffolder.php
@@ -3,57 +3,59 @@
 class Scaffolder_Widget extends WP_Widget
 {
     /**
-     * Sets up the widgets name etc
+     * Sets up the widgets name etc.
      */
     public function __construct()
     {
         parent::__construct(
             'scaffold',
             __('Scaffold', 'scaffold'),
-            array( 'description' => __( 'Scaffold a widget', 'scaffold' ), )
+            array('description' => __('Scaffold a widget', 'scaffold'))
         );
     }
 
     /**
-     * Outputs the content of the widget
+     * Outputs the content of the widget.
      *
      * @param array $args
      * @param array $instance
      */
-    public function widget( $args, $instance )
+    public function widget($args, $instance)
     {
-        $title = ( ! empty( $instance['title'] ) ) ? $instance['title'] : '';
-        $title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
+        $title = (! empty($instance['title'])) ? $instance['title'] : '';
+        $title = apply_filters('widget_title', $title, $instance, $this->id_base);
 
         echo $args['before_widget'];
-            if ( $title ) echo $args['before_title'] . $title . $args['after_title'];
+            if ($title) {
+                echo $args['before_title'].$title.$args['after_title'];
+            }
         echo $args['after_widget'];
     }
 
     /**
-     * Outputs the options form on admin
+     * Outputs the options form on admin.
      *
      * @param array $instance The widget options
      */
-    public function form( $instance )
+    public function form($instance)
     {
         $title = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
 
         ?>
 
-        <p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-        <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
+        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+        <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
 
         <?php
     }
 
     /**
-     * Processing widget options on save
+     * Processing widget options on save.
      *
      * @param array $new_instance The new options
      * @param array $old_instance The previous options
      */
-    public function update( $new_instance, $old_instance )
+    public function update($new_instance, $old_instance)
     {
         $instance = $old_instance;
         $instance['title'] = strip_tags($new_instance['title']);
